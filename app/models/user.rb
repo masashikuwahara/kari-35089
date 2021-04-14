34896eec93a05d9d1136b27e61ba3d067b3d8b0e
桑原雅史 <brucemclarenp4@gmail.com>
2021-04-14 23:53:04 +0900

    投稿をDBに反映

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :name, presence: true
-         validates :explanation, presence: true
-         validates :impression, presence: true
+         validates :favorite, presence: true
+         validates :museum, presence: true
          has_many :pictures
 end
--- db/migrate/20210405114406_create_pictures.rb
+++ db/migrate/20210405114406_create_pictures.rb
@@ -2,9 +2,10 @@ class CreatePictures < ActiveRecord::Migration[6.0]
   def change
     create_table :pictures do |t|
 
-      t.string :title
-      t.text   :explanation
-      t.text   :impression
+      t.string     :title
+      t.text       :explanation
+      t.text       :impression
+      t.references :user,       foreign_key: true
       t.timestamps
     end
   end
