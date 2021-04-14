eb526fbbfca1b4313d93922d3aae9dd52bf515f0
桑原雅史 <brucemclarenp4@gmail.com>
2021-04-15 00:05:49 +0900

    indexに表示

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,7 +2,7 @@ class PicturesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    
+    @pictures = Picture.includes(:user)
   end
 
   def new
