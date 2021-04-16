8ef0676473e517871fac7ad57cf422f959de8a6b
桑原雅史 <brucemclarenp4@gmail.com>
2021-04-16 23:30:03 +0900

    編集機能追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -16,11 +16,26 @@ class PicturesController < ApplicationController
     else
       render :new
     end
-     
   end
 
   def show
     @picture = Picture.find(params[:id])
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
+    unless @picture.user_id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    picture = Picture.find(params[:id])
+    if picture.update(picture_params)
+      redirect_to picture_path
+    else
+      render :edit
+    end
   end
 
   def search
