0dd34283990ce574763af28bc78352c7fdd6bb7b
桑原雅史 <brucemclarenp4@gmail.com>
2021-04-27 17:16:44 +0900

    単体コード実装

--- spec/factories/pictures.rb
+++ spec/factories/pictures.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :picture do
-    
+    title                 {'タイトル'}
+    explanation           {'説明文'}
+    impression            {'感想'}
+
+
+    association :user
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test.jpg'), filename: 'test.jpg')
+    end
   end
 end
--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Picture, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @picture = FactoryBot.build(:picture)
+  end
+
+  describe '画像投稿機能' do
+    context '画像投稿できる' do
+      it '内容が正しければ投稿できる' do
+        expect(@picture).to be_valid
+      end
+    end
+
+    context '画像投稿できない時' do
+    it '画像が空だと登録できない' do
+      @picture.image = nil
+      @picture.valid?
+      expect(@picture.errors.full_messages).to include("Image can't be blank")
+    end
+    it 'タイトルが空だと登録できない' do
+      @picture.title = ''
+      @picture.valid?
+      expect(@picture.errors.full_messages).to include("Title can't be blank")
+    end
+    it 'タイトルの説明が空だと登録できない' do
+      @picture.explanation = ''
+      @picture.valid?
+      expect(@picture.errors.full_messages).to include("Explanation can't be blank")
+    end
+    it 'タイトルの感想が空だと登録できない' do
+      @picture.impression = ''
+      @picture.valid?
+      expect(@picture.errors.full_messages).to include("Impression can't be blank")
+    end
+  end
+end
 end
