2ad89985103f0e57f58bf0103b23b2e5154ed276
桑原雅史 <brucemclarenp4@gmail.com>
2021-05-13 10:51:46 +0900

    プロフィール編集機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :favorite, :museum])
   end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :favorite, :museum])
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,31 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:mypage]
+
   def show
     @user = User.find(params[:id])
     @pictures = @user.pictures
   end
-end
+  
+  def edit
+    @user = User.find(params[:id])
+    unless @user == current_user
+          redirect_to user_path(@user)
+       end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email, :password, :name, :favorite, :museum)
+    end
+
+  end
